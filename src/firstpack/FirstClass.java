a83e162f3936fe17410896ee7d0a4e964837adb5
Spitamin Majidi <Spitaminmajeedi@gmail.com>
2022-07-19 23:42:07 +0200

    third commit

--- src/firstpack/FirstClass.java
+++ src/firstpack/FirstClass.java
@@ -4,5 +4,6 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("this is a new update on local master branch");
+        System.out.println("This is third commit from local master");
     }
 }
