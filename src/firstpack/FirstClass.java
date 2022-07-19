995f4b33d52e3e24153ab33c64e4856608702b4d
SpitaminMajidi <109357606+SpitaminMajidi@users.noreply.github.com>
2022-07-19 23:42:46 +0200

    last from remote

--- src/firstpack/FirstClass.java
+++ src/firstpack/FirstClass.java
@@ -4,6 +4,17 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("this is a new update on local master branch");
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
          System.out.println("Remote master update");
     }
 }
