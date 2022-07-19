99a6a801ba340d172775093ce1f7c78706bd5c65
Spitamin Majidi <Spitaminmajeedi@gmail.com>
2022-07-19 23:39:46 +0200

    second from local master

--- src/firstpack/FirstClass.java
+++ src/firstpack/FirstClass.java
@@ -3,5 +3,6 @@ package firstpack;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("this is a new update on local master branch");
     }
 }
